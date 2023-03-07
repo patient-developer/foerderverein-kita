68e6bc20e7014d4da438dce1aaf6663290b6d4bd
Patient Developer <pantient.developer@posteo.de>
2023-03-07 19:52:48 +0100

    probable final version to deploy

--- src/main/java/kbh/foerdervereinkita/mvc/controller/LandingPageController.java
+++ src/main/java/kbh/foerdervereinkita/mvc/controller/LandingPageController.java
@@ -1,20 +1,38 @@
 package kbh.foerdervereinkita.mvc.controller;
 
 import kbh.foerdervereinkita.commons.Views;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequiredArgsConstructor
 public class LandingPageController {
 
+  @Value("${ASSOCIATION_LOGO_FILENAME}")
+  private final String associationEmblemFilename;
+
+  @Value("${KINDERGARTEN_LOGO_FILENAME}")
+  private final String kindergartenEmblemFilename;
+
   @GetMapping("/")
-  ModelAndView rootGet() {
+  ModelAndView rootGet(Model model) {
+
+    model.addAttribute("associationEmblemFilename", associationEmblemFilename);
+    model.addAttribute("kindergartenEmblemFilename", kindergartenEmblemFilename);
+
     return new ModelAndView(Views.INDEX);
   }
 
   @GetMapping("/index")
-  ModelAndView indexGet() {
+  ModelAndView indexGet(Model model) {
+
+    model.addAttribute("associationEmblemFilename", associationEmblemFilename);
+    model.addAttribute("kindergartenEmblemFilename", kindergartenEmblemFilename);
+
     return new ModelAndView(Views.INDEX);
   }
 }
