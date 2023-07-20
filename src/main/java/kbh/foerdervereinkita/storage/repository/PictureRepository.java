56f5458791aa05307bfcdab8159d48ca4dfaa2de
Patient Developer <pantient.developer@posteo.de>
2023-07-20 20:46:56 +0200

    refactor sort pictures by rank

--- src/main/java/kbh/foerdervereinkita/storage/repository/PictureRepository.java
+++ src/main/java/kbh/foerdervereinkita/storage/repository/PictureRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PictureRepository extends JpaRepository<PictureEntity, Long> {
 
-  Collection<PictureEntity> findAllByEventKey(String eventKey);
+  Collection<PictureEntity> findAllByEventKeyOrderByRank(String eventKey);
 }
